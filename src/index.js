8f4691d75de03734e9cd5378668b682666db93ac
aria ortko <megymikei@gmail.com>
2022-02-23 00:54:46 +0300

    all tests is ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let str = n.toString().split("");
+
+    for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+        let temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    return parseInt(str.join(""));
 }
